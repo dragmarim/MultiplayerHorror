13270443de8d7365a9dc907a59259e52e775384c
Dragmarim <dragmarim@yahoo.com>
2020-02-14 18:57:03 -0500

    Created UI scripts

---
+++ Horror Coop Game/Assets/Scripts/ChangeSliderMax.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSliderMax : MonoBehaviour
+{
+    Slider localPlayerSlider;
+
+    void Start() {
+        localPlayerSlider = GetComponent<Slider>();
+    }
+
+    public void setSliderMax(float max) {
+        localPlayerSlider.maxValue = max;
+    }
+}
---
+++ Horror Coop Game/Assets/Scripts/UpdateText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour
+{
+    Text textComponent;
+    void Start() {
+        textComponent = GetComponent<Text>();
+    }
+
+    public void SetSliderValue(float sliderValue) {
+        textComponent.text = "" + sliderValue;
+    }
+}
