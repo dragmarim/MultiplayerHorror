6321b7e14d9155ea0ced98add2e3587348f74604
Dragmarim <dragmarim@yahoo.com>
2020-02-14 20:18:31 -0500

    Added start UI

---
+++ Horror Coop Game/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float rotationSpeed = 10;
+    public float currentRotation;
+
+    void Start() {
+        currentRotation = rotationSpeed;
+    }
+
+    void Update() {
+        transform.rotation = Quaternion.Euler(30, currentRotation, 0);
+        currentRotation += rotationSpeed;
+        if (currentRotation > 360) {
+            currentRotation -= 360;
+        }
+    }
+}
--- Horror Coop Game/Assets/Scripts/Randomize.cs
+++ Horror Coop Game/Assets/Scripts/Randomize.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Randomize : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject userInterface;
+    public GameObject menuCamera;
+    public GameObject randomSeedObject;
+    public GameObject localPlayerObject;
+    public GameObject playerCountObject;
+
     public int seed = 1;
     public int localPlayer = 1;
     public int playerCount = 1;
@@ -11,6 +19,7 @@ public class Randomize : MonoBehaviour
     public List<int> playerOrder = new List<int>();
     List<int> runesAvailable = new List<int>();
     List<int> runeOrder = new List<int>();
+
     void Start()
     {
         Random.InitState(seed);
@@ -59,5 +68,14 @@ public class Randomize : MonoBehaviour
             playersAvailable.RemoveAt(random);
             Debug.Log(playerOrder[i]);
         }
+    }
+
+    public void StartGame() {
+        seed = int.Parse(randomSeedObject.GetComponent<Text>().text);
+        localPlayer = (int)localPlayerObject.GetComponent<Slider>().value;
+        playerCount = (int)playerCountObject.GetComponent<Slider>().value;
+        player.SetActive(true);
+        userInterface.SetActive(false);
+        menuCamera.SetActive(false);
     }
 }
